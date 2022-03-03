e6f59a78c85ba3562b953737d779f5a21fcc0787
Misael <misael@ip-192-168-15-5.sa-east-1.compute.internal>
2022-03-03 16:49:04 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepServiceService } from './cep-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ConsultaCep';
+
+  constructor(private cepService: CepServiceService) {}
+
+  consultaCep(valor, form){
+      this.cepService.buscar(valor).subscribe((dados) => this.popularForm2(dados, form));
+  } 
+
+  consultaCep2(uf, cidade, logradouro, form){
+    this.cepService.buscar2(uf, cidade, logradouro).subscribe((dados) => this.popularForm(dados, form))
+   console.log(uf, cidade, logradouro);
+  }
+
+  popularForm(dados, form){
+    form.setValue({
+      cep: dados[0].cep,
+      logradouro: dados[0].logradouro,
+      bairro: dados[0].bairro,
+      cidade: dados[0].localidade,
+      uf: dados[0].uf
+    })
+  }
+  popularForm2(dados, form){
+    form.setValue({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      uf: dados.uf
+    })
+  }
+  resetaDadosForm(form){
+    form.setValue({
+      cep: null,
+      logradouro: null,
+      bairro: null,
+      cidade: null,
+      uf: null
+    })
+  }
+
 }
